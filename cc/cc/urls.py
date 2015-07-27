4458a7b4de6b2c9a4e0bdd5c0a0c82990018206b
Rogov Viktor <vtk666@gmail.com>
2015-07-28 00:30:41 +0600

    Registration

--- cc/cc/urls.py
+++ cc/cc/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+import registration.backends.default.urls
+import registration.auth_urls
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('goods.urls')),
-    url(r'^', include('django.contrib.auth.urls'))
+    url(r'^accounts/', include('registration.backends.default.urls')),
 ]
