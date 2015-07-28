4f639273d97c3b1168be61b3fb438052bb57457e
Rogov Viktor <vtk666@gmail.com>
2015-07-28 16:09:23 +0600

    Handle DivisionByZero

--- cc/goods/models.py
+++ cc/goods/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import decimal
 
 class Unit(models.Model):
     title = models.CharField(max_length=200)
@@ -31,39 +32,24 @@ class Good(models.Model):
         return res
 
     def list_costs(self):
-        """
-        $id = (int)$id;
-        $node = $this->db->selectRow("SELECT * FROM goods WHERE id={$id}");
-        if ($node['packed']) {
-            return $this->db->select(
-                "SELECT b.title, a.timestamp, ROUND(a.cost/a.amount, 2) as pack_cost, ROUND(a.cost/a.amount/sg.pack_volume, 2) as unit_cost
-               FROM goods g
-                    JOIN sales a ON g.id=a.good_id
-                    JOIN goods sg ON a.good_id=sg.id
-                    JOIN shops b ON a.shop_id=b.id
-              WHERE {$node['node_left']}<=g.node_left AND g.node_right<={$node['node_right']}
-           ORDER BY a.timestamp DESC"
-            );
-        } else {
-            return $this->db->select(
-                "SELECT b.title, a.timestamp, ROUND(a.cost/a.amount/sg.pack_volume, 2) as unit_cost
-               FROM goods g
-                    JOIN sales a ON g.id=a.good_id
-                    JOIN goods sg ON a.good_id=sg.id
-                    JOIN shops b ON a.shop_id=b.id
-              WHERE {$node['node_left']}<=g.node_left AND g.node_right<={$node['node_right']}
-           ORDER BY a.timestamp DESC"
-            );
-        }
-        """
         if self.packed:
             def cb(cost):
-                cost.pack_cost = "%.2f" % (cost.cost/cost.amount)
-                cost.unit_cost = "%.2f" % (cost.cost/cost.amount/cost.good.pack_volume)
+                try:
+                    cost.pack_cost = "%.2f" % (cost.cost/cost.amount)
+                except decimal.DivisionByZero:
+                    cost.pack_cost = 'n/a'
+
+                try:
+                    cost.unit_cost = "%.2f" % (cost.cost/cost.amount/cost.good.pack_volume)
+                except decimal.DivisionByZero:
+                    cost.unit_cost = 'n/a'
                 return cost
         else:
             def cb(cost):
-                cost.unit_cost = "%.2f" % (cost.cost/cost.amount/cost.good.pack_volume)
+                try:
+                    cost.unit_cost = "%.2f" % (cost.cost/cost.amount/cost.good.pack_volume)
+                except decimal.DivisionByZero:
+                    cost.unit_cost = 'n/a'
                 return cost
         return self._traverse_costs(self, cb)
 
